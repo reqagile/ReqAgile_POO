3c6718851c01ba5327582688df51bdabdc426b5e
Eduardo Scartezini Correia Carvalho <140137084@aluno.unb.br>
2015-09-17 10:29:05 -0300

    C

--- src/mvc/model/connectDB/SqlUser.java
+++ src/mvc/model/connectDB/SqlUser.java
@@ -1,5 +1,6 @@
 package mvc.model.connectDB;
 
+import java.sql.SQLException;
 
 /**
  * Classe que implementa a conexão com a tabela user 
@@ -8,37 +9,75 @@ package mvc.model.connectDB;
  * @author Eduardo Scartezini
  *
  */
-public class SqlUser implements ConnectionUser {
+public class SqlUser implements ConnectionUser {	
+	
+	/**
+	 * Construtor do SqlUser. 
+	 * 
+	 * 		Se não estiver conectado ao DataBase ele conecta.
+	 */
+	public SqlUser(){
+		int i = 0;
+		while(MySql.isConnect() == false && i < 10){
+			MySql.connect();
+			i++;
+		}
+	}
+	@Override
+	public boolean insertRegisty(String name, String login, String password, String email) {
+		String query = "IMSERT INTO user VALUES "+"(NULL,'"+name+"','"+login+"','"+password+"','"+email+"');";
+		try{
+			MySql.stm.executeUpdate(query);
+			return true;
+		}catch(SQLException e){
+			return false;
+		}
+	}
+	
+	
+	@Override
+	//Comoe retornar um vetor de string?
+	//TODO retornar um vetor de string com todas informações do registro; 
+	public String selectRegisty(int iduser) {
+		String query = "SELECT FROM user WHERE iduser ="+iduser+";";
+		try {
+			MySql.stm.executeUpdate(query);
+			if(MySql.rs.next()){
+				return MySql.rs.getString("name");
+			}else{
+				return null;
+			}
+		} catch (SQLException e) {
+			return null;
+		}
+	}
+	
+	@Override
+	//TODO retornar um vetor de string com todas informações do registro; 
+	public String selectRegisty(String login) {
+		String query = "SELECT FROM user WHERE login ='"+login+"';";
+		try {
+			MySql.stm.executeUpdate(query);
+			if(MySql.rs.next()){
+				return MySql.rs.getString("name");
+			}else{
+				return null;
+			}
+		} catch (SQLException e) {
+			return null;
+		}
+	}
+	
+	
 
 	@Override
-	public boolean InsertRegisty(String name, String login, String password, String email) {
+	public boolean deleteRegisty(int iduser) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
-	
-	@Override
-	public String SelectRegisty(int iduser) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public String SelectRegisty(String login) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
 
 	@Override
-	public boolean DeleteRegisty(int iduser) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean AlterRegisty(int iduser) {
+	public boolean alterRegisty(int iduser) {
 		// TODO Auto-generated method stub
 		return false;
 	}
